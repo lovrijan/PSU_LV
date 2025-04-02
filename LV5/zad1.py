7f278f201c0d6f0652ae9ad26672090175249df9
lovrijan <92103513+lovrijan@users.noreply.github.com>
2025-04-02 11:46:23 +0200

    Add files via upload

---
+++ LV5/zad1.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# ucitaj podatke za ucenje
+df = pd.read_csv('C:\\Users\\student\\Downloads\\occupancy_processed.csv')
+
+feature_names = ['S3_Temp', 'S5_CO2']
+target_name = 'Room_Occupancy_Count'
+class_names = ['Slobodna', 'Zauzeta']
+
+X = df[feature_names].to_numpy()
+y = df[target_name].to_numpy()
+
+# Scatter plot
+plt.figure()
+for class_value in np.unique(y):
+    mask = y == class_value
+    plt.scatter(X[mask, 0], X[mask, 1], label=class_names[class_value])
+
+plt.xlabel('S3_Temp')
+plt.ylabel('S5_CO2')
+plt.title('Zauzetost prostorije')
+plt.legend()
+plt.show()
