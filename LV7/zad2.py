3219b4345045848d072a0645eb527093c55fbae9
lovrijan <92103513+lovrijan@users.noreply.github.com>
2025-04-15 13:05:59 +0200

    Create zad2.py

---
+++ LV7/zad2.py
@@ -0,0 +1,7 @@
+import numpy as np
+from tensorflow import keras
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.models import Sequential
+from matplotlib import pyplot as plt
+from sklearn.metrics import confusion_matrix, accuracy_score
+import seaborn as sns
